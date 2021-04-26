8878853f4abb1c028f1d974b60056d912cd71cd3
divij <divij@uw.edu>
2021-04-25 20:12:16 -0700

    Initial Commit

---
+++ Life Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Life Counter
+//
+//  Created by Divij Satija on 4/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
